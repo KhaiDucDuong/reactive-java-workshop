c3371f3af527da1599319a13bb3ee230d553a211
KhaiDucDuong <greenjohnny135@gmail.com>
2025-08-24 23:12:33 +0700

    solve problem 1->4 exercise 1

--- src/main/java/io/javabrains/reactiveworkshop/Exercise1.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise1.java
@@ -8,16 +8,32 @@ public class Exercise1 {
 
         // Print all numbers in the intNumbersStream stream
         // TODO: Write code here
+        System.out.println("PROBLEM 1");
+        StreamSources.intNumbersStream().forEach(number -> System.out.println(number));
 
         // Print numbers from intNumbersStream that are less than 5
         // TODO: Write code here
+        System.out.println("PROBLEM 2");
+        StreamSources.intNumbersStream().filter(num -> num < 5).forEach(number -> System.out.println(number));
 
         // Print the second and third numbers in intNumbersStream that's greater than 5
         // TODO: Write code here
+        System.out.println("PROBLEM 3");
+        StreamSources.intNumbersStream().filter(num -> num > 5)
+                .skip(1)
+                .limit(2)
+                .forEach(num -> System.out.println(num));
 
         //  Print the first number in intNumbersStream that's greater than 5.
         //  If nothing is found, print -1
         // TODO: Write code here
+        System.out.println("PROBLEM 4");
+        StreamSources.intNumbersStream()
+                .filter(num -> num > 5)
+                .findFirst().ifPresentOrElse(
+                        num -> System.out.println(num),
+                        () -> System.out.println(-1)
+                );
 
         // Print first names of all users in userStream
         // TODO: Write code here
